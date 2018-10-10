7c0898611dbf8ac7c347ec91e968dbd77161787e
Wasif Khan <waasif.khaan@gmail.com>
2018-10-10 21:26:34 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ActiveRecord::Base
   # add associations here
   belongs_to :artist 
   belongs_to :genre
+  has_many :notes
   
    def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
