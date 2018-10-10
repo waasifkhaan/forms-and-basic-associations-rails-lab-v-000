ccaaf65e6fa8fc0f027ef4164f8fa6999a05a226
Wasif Khan <waasif.khaan@gmail.com>
2018-10-10 21:29:10 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,11 +2,4 @@ class Genre < ActiveRecord::Base
   # add associations
   has_many :songs
   
-  def genre_name=(genre_name)
-    self.artist = Genre.find_or_create_by(genre_name)
-  end 
-  
-  def genre_name
-    self.genre ? self.genre.name : nil
-  end 
-end
+  end
